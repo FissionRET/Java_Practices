2bf60e43729131b0dfbfa83cc6357d729ebff1d8
Zord <quangsang.red@gmail.com>
2024-10-31 09:10:26 +0700

    updoot

---
+++ PhamQuangSang/hvu.em.loe/src/App.java
@@ -0,0 +1,10 @@
+import hvu.em.loe.Motor;
+import hvu.hrm.fte.Staff;
+
+public class App {
+    public static void main(String[] args) {
+        // Motor.run();
+        Staff.run();
+    }
+
+}
---
+++ PhamQuangSang/hvu.em.loe/src/hvu/hrm/fte/Staff.java
@@ -0,0 +1,53 @@
+package hvu.hrm.fte;
+
+public class Staff {
+    private Boolean isLeturer;
+    private int id;
+
+    // conn
+    public Staff() {
+    }
+    public Staff(Boolean isLeturer, int id) {
+        this.isLeturer = isLeturer;
+        this.id = id;
+    }
+
+    // getter, setter
+    public Boolean getIsLeturer() {
+        return isLeturer;
+    }
+
+    public void setIsLeturer(Boolean isLeturer) {
+        this.isLeturer = isLeturer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // other methods
+    public void display() {
+        System.out.println("Staff: " + isLeturer + " " + id);
+    }
+
+    // run (Main)
+    public static void run(){
+        Staff staff1 = new Staff();
+        Staff staff2 = new Staff(true, 1);
+
+        System.out.println("Staff 1:");
+        staff1.display();
+        System.out.println("Staff 2:");
+        staff2.display();
+
+        staff1.setIsLeturer(true);
+        staff1.setId(2);
+
+        System.out.println("Staff 1 after update:");
+        staff1.display();
+    }
+}
