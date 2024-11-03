cb00a8b843045691fda0786a0dcafe7d1695dcf2
ChinoTheGod <meodihia756@gmail.com>
2024-11-03 14:38:56 +0700

    Swing UI Frame Demo

---
+++ PTK/src/gui/FrameDemo.java
@@ -0,0 +1,42 @@
+package gui;
+
+import javax.swing.*;
+
+public class FrameDemo {
+    private JTextField txtFirstName;
+    private JTextField txtLastName;
+    private JTextField txtTitle;
+    private JTextField txtNickName;
+    private JPanel namePanel;
+    private JPanel mainPanel;
+    private JLabel lbFirstName;
+    private JLabel lbLastName;
+    private JLabel lbTitle;
+    private JLabel lbNickName;
+    private JPanel emailPanel;
+    private JTextField txtEmailAddress;
+    private JLabel lbEmailAddress;
+    private JButton btnAdd;
+    private JButton btnEdit;
+    private JButton btnDelete;
+    private JTextArea txtAreaItems;
+    private JPanel mailFormatPanel;
+    private JRadioButton radioBtnHTML;
+    private JRadioButton radioBtnPlainText;
+    private JRadioButton radioBtnCustom;
+    private JButton btnOK;
+    private JButton btnExit;
+    private JPanel buttonsPanel;
+
+    public FrameDemo() {
+
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Frame Demo");
+        jFrame.setContentPane(new FrameDemo().mainPanel);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+}
